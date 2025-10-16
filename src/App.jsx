6705510c7af0648900f1c1d65afa897fa6224a4c
Nikolay Ignatov <ign.nikolay@gmail.com>
2025-10-16 19:32:13 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-lime-500">
+        There are three bags, each containing two marbles. Bag A contains two white marbles, Bag B contains two black marbles and Bag C contains one white and one black marble. You pick a random bag and take out white marble. What is the probability that the remaining marble from the same bag is also white?
+      </p>
+      <p className="text-red-600 font-mono italic">
+        There are three bags, each containing two marbles. Bag A contains two white marbles, Bag B contains two black marbles and Bag C contains one white and one black marble. You pick a random bag and take out white marble. What is the probability that the remaining marble from the same bag is also white?
+      </p>
+    </>
+  )
+}
+
+export default App
